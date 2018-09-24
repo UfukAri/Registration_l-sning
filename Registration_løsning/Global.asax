c14d39a552a1603d9df80c1810f350cdc2829b88
Ufuk Ari <s305519@stud.hioa.no>
2018-09-24 14:06:23 +0200

    Add project files.

---
+++ Registration_løsning/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Registration_løsning.MvcApplication" Language="C#" %>
---
+++ Registration_løsning/Models/UserAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Registration_løsning.Models
+{
+    public class UserAccount
+    {
+
+        [Key]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage = "Feltet må fylles inn*.")]
+        public String Firstname { get; set; }
+
+
+        [Required(ErrorMessage = "Feltet må fylles inn*.")]
+        public String Lastname { get; set; }
+
+
+        [Required(ErrorMessage = "Feltet må fylles inn*.")]
+        [RegularExpression(@"^[A-Za-z0-9][-a-z0-9.!#$%&'*+-=?^_`{|}~\/]+@([-a-z0-9]+\.)+[a-z]{2,5}$", ErrorMessage = "Skriv inn korrekt email.")]
+        public String Email { get; set; }
+
+
+        [Required(ErrorMessage = "Feltet må fylles inn*.")]
+        [RegularExpression(@"^[A-Za-z] [A-Za-z0-9._]{5,14}$", ErrorMessage = "Skriv inn Korrekt brukernavn.")]
+        public String Username { get; set; }
+
+
+        [Required(ErrorMessage = "Feltet må fylles inn*.")]
+        [RegularExpression(@"^[A-Za-z0-9]{6,15}$", ErrorMessage = "Skriv inn Korrekt Passord.")]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Ikke gjentatt passord skrevet")]
+        [DataType(DataType.Password)]
+        public String ConfirmPassword { get; set; }
+    }
+}
